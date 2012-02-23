2572a1019c2bac886e24b9ec08215a66c14a5e6b
Cong Teng <congteng45@gmail.com>
2012-02-23 11:17:31 +0800

    xx

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.1'
+
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  # gem 'therubyracer'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '6e8c5750e2f5f4e954929829e431b675df82b8a6c7f5ed76fac17362fc5c7b9028f3b9db1111c845fa034f707fef2c7ce78ae3c14ec213f82ffb3734595323fa'
